cb2edaeddc8e4dd616382f9a7f7ff76942415c89
Zahra Waheed <156197046+zahra-waheed53@users.noreply.github.com>
2025-12-15 14:43:49 +0500

    Enable GPU support for model loading and prediction
    Updated model loading and prediction to use GPU if available.

--- model.py
+++ model.py
@@ -6,22 +6,24 @@ import os
 
 MODEL_PATH = os.getenv("MODEL_PATH", "checkpoints/foodnet.pth")
 
+# Use GPU if available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 def load_model():
-    checkpoint = torch.load(MODEL_PATH, map_location="cpu")
+    checkpoint = torch.load(MODEL_PATH, map_location=device)
 
     model = models.mobilenet_v2(weights=None)
     num_classes = len(checkpoint["classes"])
-
     model.classifier[1] = torch.nn.Linear(
         model.classifier[1].in_features, num_classes
     )
 
     model.load_state_dict(checkpoint["model_state"])
+    model.to(device)
     model.eval()
 
     model.classes = checkpoint["classes"]
     return model
-
 
 def predict_image_class(model, img_bytes):
     transform = transforms.Compose([
@@ -30,7 +32,7 @@ def predict_image_class(model, img_bytes):
     ])
 
     image = Image.open(io.BytesIO(img_bytes)).convert("RGB")
-    image = transform(image).unsqueeze(0)
+    image = transform(image).unsqueeze(0).to(device)  # move to same device as model
 
     with torch.no_grad():
         outputs = model(image)
